d3b4de6e878634330a673516792d4065b051491c
eichdmk <imutsaev95@gmail.com>
2026-02-21 11:56:33 +0300

    debounce

--- frontend/src/contexts/TermsStatusContext.tsx
+++ frontend/src/contexts/TermsStatusContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useCallback } from "react"
+import { createContext, useContext, useState } from "react"
 
 export type TermsStatus = "Сохранено" | "Сохраняется…" | "Несохранено" | null
 
